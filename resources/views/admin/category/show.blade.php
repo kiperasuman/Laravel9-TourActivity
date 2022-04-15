2307971b4408466f86493cca1bc39aafcff1a1da
kiperasuman <asuman.kiper00@gmail.com>
2022-04-15 22:06:29 +0300

    Show Data Implementation Category

---
+++ resources/views/admin/category/show.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.adminbase')
+
+@section('title','Edit Category')
+
+@section('content')
+    <!-- partial -->
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title" >{{$data->title}}</h1>
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <tr>
+                                     <th style="width: 100px" >Id</th>
+                                     <td>{{$data->id}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th style="width: 100px">Title</th>
+                                        <td>{{$data->title}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th style="width: 100px">Keyword</th>
+                                        <td>{{$data->keywords}}</td>
+                                    </tr>
+
+                                    <tr> <th style="width: 100px">Description</th>
+                                        <td>{{$data->description}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Image</th>
+                                        <td>  </td>
+                                    </tr>
+                                    <tr>
+                                        <th style="width: 100px">Status</th>
+                                        <td>{{$data->status}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th style="width: 100px">Created Date</th>
+                                        <td>{{$data->created_at}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th style="width: 100px">Updated Time</th>
+                                        <td>{{$data->updated_at}}</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="/admin/category/edit/{{$data->id}}"class="btn btn-outline-success btn-fw"> Edit</a>
+                <a href="/admin/category/destroy/{{$data->id}}"class="btn btn-outline-reddit btn-fw" onclick="return confirm('Deleting!Are you sure?')"> Delete </a>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -56,5 +56,6 @@ Route::get('/admin',[AdminHomeController::class,'index'])->name('index');
 Route::get('/admin/category',[AdminCategoryController::class,'index'])->name('admin_category');
 Route::get('/admin/category/create',[AdminCategoryController::class,'create'])->name('admin_category_create');
 Route::post('/admin/category/store',[AdminCategoryController::class,'store'])->name('admin_category_store');
-Route::get('/admin/category/edit{id}',[AdminCategoryController::class,'edit'])->name('admin_category_edit');
+Route::get('/admin/category/edit/{id}',[AdminCategoryController::class,'edit'])->name('admin_category_edit');
 Route::post('/admin/category/update/{id}',[AdminCategoryController::class,'update'])->name('admin_category_update');
+Route::get('/admin/category/show/{id}',[AdminCategoryController::class,'show'])->name('admin_category_show');
